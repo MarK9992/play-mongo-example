c87a1181b6bb142b4e5b681f529c98a4fb8072f6
Marc Karassev <marc.karassev@yahoo.fr>
2016-10-03 12:43:42 +0200

    Person JSON serialization / deserialization

--- app/models/Address.scala
+++ app/models/Address.scala
@@ -1,11 +1,11 @@
 package models
 
-/**
- * Created by fred on 17/04/2015.
- */
-case class Address(street : String, town:String, zipCode:String)
+import play.api.libs.json.Json
 
+case class Address(street: String, town: String, zipCode: String)
 
-object Address{
-  //TODO json Format
+object Address {
+
+  implicit val addressFormat = Json.format[Address]
+
 }
--- app/models/person.scala
+++ app/models/person.scala
@@ -1,10 +1,8 @@
 package models
 
-import org.joda.time.{Years, DateTime}
-
-/**
- * @author Marc Karassev
- */
+import org.joda.time.{DateTime, Years}
+import play.api.libs.json.Json.JsValueWrapper
+import play.api.libs.json._
 
 sealed trait Sex
 case object male extends Sex
@@ -27,13 +25,60 @@ case class Person(name: String, lastName: String, birthDate: DateTime, sex: Sex,
 
 
 object Person {
-  //TODO hint : créer le Json Read et Write (ou Format)
+
+  implicit val addressFormat = Address.addressFormat
+  implicit val addressTypeFormat = AddressType.AddressTypeFormat
+  implicit object addressesFormat extends Format[Map[AddressType, Address]] {
+
+    override def reads(json: JsValue): JsResult[Map[AddressType, Address]] = json.asOpt[Map[AddressType, Address]] match {
+      case Some(value)  => JsSuccess(value)
+      case None         => JsError("could not parse: " + json)
+    }
+
+    override def writes(o: Map[AddressType, Address]): JsValue = Json.obj(
+      o.map {
+        case (addressType, address) =>
+          val ret: (String, JsValueWrapper) = addressTypeFormat.writes(addressType).toString -> addressFormat.writes(address)
+          ret
+      }.toSeq:_*
+    )
+
+  }
+
+  implicit val sexFormat = Sex.SexFormat
+
+  implicit val personFormat = Json.format[Person]
+
 }
 
 object AddressType {
-  //TODO hint : créer le Json Read et Write (ou Format)
+
+  implicit object AddressTypeFormat extends Format[AddressType] {
+
+    override def reads(json: JsValue): JsResult[AddressType] = json match {
+      case JsString("personal")      => JsSuccess(personal)
+      case JsString("professional")  => JsSuccess(professional)
+    case _                           => JsError("could not parse: " + json)
+    }
+
+    override def writes(o: AddressType): JsValue = JsString(o.toString)
+
+  }
+
 }
 
 object Sex {
-  //TODO hint : créer le Json Read et Write (ou Format)
+
+  implicit object SexFormat extends Format[Sex] {
+
+    override def reads(json: JsValue): JsResult[Sex] = json match {
+      case JsString("male")    => JsSuccess(male)
+      case JsString("female")  => JsSuccess(female)
+      case _                   => JsError("could not parse: " + json)
+    }
+
+    override def writes(o: Sex): JsValue = JsString(o.toString)
+
+  }
+
 }
